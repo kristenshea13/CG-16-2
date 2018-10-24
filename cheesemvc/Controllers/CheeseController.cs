fc0147074dcb43647fd5b0266d942b370e89b5c1
Kristen Shea <kstar61@live.com>
2018-10-23 22:01:04 -0500

    *

---
+++ cheesemvc/Controllers/CheeseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace cheesemvc.Controllers
+{
+    public class CheeseController : Controller
+    {
+        static private List<string> Cheeses = new List<string>();
+
+        public IActionResult Index()
+        {
+            ViewBag.cheeses = Cheeses;
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name)
+        {
+            Cheeses.Add(name);
+            return Redirect("/Cheese");
+        }
+    }
+}
